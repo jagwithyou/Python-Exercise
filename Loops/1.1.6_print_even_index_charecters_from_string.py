f486fb41e249a892ac0046b95359548b68e94401
jagwithyou <jagwithyou@gmail.com>
2022-10-08 16:17:01 +0530

    Python Exercise - Print characters from a string that are present at an even index number using for loop

--- Loops/1.1.6_print_even_index_charecters_from_string.py
+++ Loops/1.1.6_print_even_index_charecters_from_string.py
@@ -4,7 +4,9 @@ WAP to Print characters from a string that are present at an even index number u
 input : "Welcome"
 Expected Output : loe
 
-Hint: suppose there is a string "Hello" to find the index of any element (Suppose "H") you can use index()
+Hint: 
+1. for lool can be used directly to iterate through a string, which will iterate through each charecters.
+2. suppose there is a string "Hello" to find the index of any element (Suppose "H") you can use index()
 i.e "Hello".index("H") => 0
 
 Sudo Code:
