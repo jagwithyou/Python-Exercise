493090cdd66506f4b5aed6f05f408cf91ec6afb4
jagwithyou <jagwithyou@gmail.com>
2022-10-15 16:33:52 +0530

    Deleted the old

---
+++ what_is_programming/1.1.1_traffic.py
@@ -0,0 +1,9 @@
+# WAP to get the traffic light color from a input and print the decission
+light_color = input("Enter the color of the traffic light - ")
+
+if light_color == "Red":
+    print("Stop")
+elif light_color == "Orange":
+    print("Be Ready")
+elif light_color == "Green":
+    print("Go")
